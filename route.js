190392e1efcbd303a647c713b71101d3d198d012
Ishaa <ishaa481999@gmail.com>
2023-04-09 09:50:26 +0530

    react backend

--- route.js
+++ route.js
@@ -4,5 +4,6 @@ route.get("/bollywood",data.bollywood);
 route.get("/book",data.book);
 route.get("/hollywood",data.hollywood);
 route.get("/gardening",data.gardening);
+route.get("/food",data.food);
 
 module.exports=route
